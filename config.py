5d2d9977ed4b057a2952e77b8e1f79964a85192a
Wkiper03 <65402963+Wkiper03@users.noreply.github.com>
2022-02-16 15:30:34 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+#Токен вашего бота
+bot_token = '5284786592:AAHjabZstqYu4n0_onapOjbOG12GKyDtrp4'
+#ID админа
+admin = 5208698204
+#Юзер саппорта
+support = '@DrugTvoy228'
+#Канал с отзывами
+#feedback = 'https://t.me/+vBvxwpLJlhI2Nzgy'
+#Название магазина
+shop_name = '🩸DruGSZshop 💊💉'
